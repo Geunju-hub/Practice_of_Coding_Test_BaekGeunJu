6fcb8753f9425152f2281efe030c01c5d08eeb5e
ever4blue <79121744+ever4blue@users.noreply.github.com>
2021-09-14 16:29:01 +0900

    BaekJoon - 15651 (백트래킹 기초)

---
+++ BaekJoon - 15651.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int N, M;
+int arr[8] = { 0, };
+int used[8 + 1] = { 0, };
+
+void func(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+	else {
+		for (int i = 1; i <= N; i++) {
+			if (used[i] == 0) {
+				arr[cnt] = i;
+				used[i] = 1;
+				func(cnt + 1);
+				used[i] = 0;
+			}
+			else {
+				arr[cnt] = i;
+				func(cnt + 1);
+			}
+		}
+	}
+}
+
+int main(void) {
+	scanf("%d %d", &N, &M);
+	func(0);
+	return 0;
+}
