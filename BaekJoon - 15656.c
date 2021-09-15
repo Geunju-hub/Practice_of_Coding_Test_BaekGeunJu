9ce3891b28ad2a37ab9d8a8565f25a2f59305f45
ever4blue <79121744+ever4blue@users.noreply.github.com>
2021-09-15 20:09:10 +0900

    BaekJoon - 15656 (백트래킹 기초)

---
+++ BaekJoon - 15656.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+int N, M;
+int arr[8] = { 0, };
+int res[8] = { 0, };
+int used[10000 + 1] = { 0, };
+
+int compare(const void *a, const void *b) {
+	if (*(int *)a > *(int *)b) {
+		return 1;
+	}
+	else if (*(int *)a == *(int *)b) {
+		return 0;
+	}
+	else {
+		return -1;
+	}
+}
+
+void func(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++) {
+			printf("%d ", res[i]);
+		}
+		printf("\n");
+		return;
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			if (used[arr[i]] == 0) {
+				res[cnt] = arr[i];
+				used[arr[i]] = 1;
+				func(cnt + 1);
+				used[arr[i]] = 0;
+			}
+			else {
+				res[cnt] = arr[i];
+				func(cnt + 1);
+			}
+		}
+	}
+}
+
+int main(void) {
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	qsort(arr, N, sizeof(int), compare);
+	func(0);
+	return 0;
+}
